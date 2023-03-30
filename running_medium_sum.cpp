7b92ff84fb42629ba41a24d3d16f263a891be612
yasantha <niroshanyi@gmail.com>
2023-03-30 15:34:31 +0530

    main function defined all Good !

--- running_medium_sum.cpp
+++ running_medium_sum.cpp
@@ -41,3 +41,19 @@ void printing_array(vector<int> array, int size) {
     }
     cout << endl;
 }
+
+
+int main() {
+    vector<int> new_median_array;
+    vector<int> number_array = {7,3,5,2};
+    int size = number_array.size();
+
+    for(int i = 0; i < size; i++) {
+        new_median_array.push_back(number_array[i]);
+        vector<int> sorted = sort(new_median_array, i+1);
+        printing_array(sorted, i+1);
+        cout <<std::fixed << std::setprecision(1)<< median(sorted, i+1) << endl;
+    }
+
+    return 0;
+}
